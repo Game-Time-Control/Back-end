9bee55caa5ef27ac90e50811500846109ccd5f88
Lorenzo <lorenzofman@gmail.com>
2020-11-13 16:54:16 -0300

    Get route from environment

--- lib/server.ts
+++ lib/server.ts
@@ -3,41 +3,45 @@ import * as mongoose from "mongoose";
 import {createSchema, Type, typedModel} from "ts-mongoose";
 const uri = "mongodb+srv://lorenzofman:TeOzakxnrHciqWDO@cluster0.1balv.mongodb.net/test?retryWrites=true&w=majority";
 
-const connection = mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, function (err)
-{
-   if (err)
-   {
-      console.log("Error connecting to Atlas");
-   }
-   else
-   {
-      console.log("Successful Connection");
-   }
-});
-
-const childSchema = createSchema
-({
-   name: Type.string( {required:true} ),
-   parentId: Type.number( {required:true} )
-});
-
-const childModel = mongoose.model('Children', childSchema);
-const childDoc = new childModel({ name: 'Steve Jobs', parentId: 42 });
-
 app.get('/', (request, response) =>
 {
    response.send('Hello world!');
 });
-app.listen(80);
 
-childDoc.save(function (err)
-{
-   if (err)
-   {
-      console.log("Failed to save");
-   }
-   else
-   {
-      console.log("Saved");
-   }
-});
+const port = process.env.PORT || 4242;
+console.log(`Start listening to ${port}`);
+app.listen(port);
+
+// const connection = mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, function (err)
+// {
+//    if (err)
+//    {
+//       console.log("Error connecting to Atlas");
+//    }
+//    else
+//    {
+//       console.log("Successful Connection");
+//    }
+// });
+//
+// const childSchema = createSchema
+// ({
+//    name: Type.string( {required:true} ),
+//    parentId: Type.number( {required:true} )
+// });
+//
+// const childModel = mongoose.model('Children', childSchema);
+// const childDoc = new childModel({ name: 'Steve Jobs', parentId: 42 });
+
+
+// childDoc.save(function (err)
+// {
+//    if (err)
+//    {
+//       console.log("Failed to save");
+//    }
+//    else
+//    {
+//       console.log("Saved");
+//    }
+// });
