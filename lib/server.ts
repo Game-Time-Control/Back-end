94de9ab3c5e2246c7069e6f4f7d31606d5740e0e
julia <juliagabriela98@gmail.com>
2020-11-27 19:06:14 -0300

    trying to fix the problem with CORS

--- lib/server.ts
+++ lib/server.ts
@@ -10,6 +10,14 @@ const database = 'development';
 const uri = `mongodb+srv://${username}:${password}@cluster0.1balv.mongodb.net/${database}?retryWrites=true&w=majority`;
 const port = process.env.PORT || 4242;
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", '*');
+   res.header("Access-Control-Allow-Credentials", "true");
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+   next();
+});
+
 async function run(): Promise<void>
 {
    await mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).catch(error => console.log(error));
