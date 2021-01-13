c1b1dbcaef112b4d896b6234478b357118e77da3
Lorenzo <lorenzofman@gmail.com>
2021-01-13 12:35:22 -0300

    Safe Guard Logs
    Add strategic logs to debug possible future crashes on Heroku
    Removes cors usage

--- lib/server.ts
+++ lib/server.ts
@@ -1,3 +1,4 @@
+console.log(`Wakey wakey!`);
 import app from "./config/app";
 import * as mongoose from "mongoose";
 import {childRoutes} from "./resources/child/childRoutes";
@@ -12,23 +13,29 @@ const database = 'development';
 const uri = `mongodb+srv://${username}:${password}@cluster0.1balv.mongodb.net/${database}?retryWrites=true&w=majority`;
 const port = process.env.PORT || 4242;
 
-const cors = require('cors');
-const fs = require('fs');
-
-app.use(cors())
-
 async function run(): Promise<void>
 {
+   console.log(`Server started running`);
    await mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).catch(error => console.log(error));
+   console.log(`Mongoose connected`);
+
    const connection = mongoose.connection;
-   // await clearDatabase();
-   // console.log(`Database cleared`);
-   // await createSample();
+
    parentRoutes(app);
+   console.log(`Parents routes configured!`);
+   
    childRoutes(app);
+   console.log(`Child routes configured!`);
+   
    gameRoutes(app);
+   console.log(`Game routes configured!`);
+   
    binaryRoutes(app);
+   console.log(`Binary routes configured!`);
+   
    genericRoute(app, connection);
+   console.log(`Generic routes configured!`);
+   
    console.log(`Start listening to ${port}`);
    app.listen(port);
 }
