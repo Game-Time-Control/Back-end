47b0b27cba6b00c30ad22def786c727fcc0483b3
julia <juliagabriela98@gmail.com>
2021-01-13 18:33:29 -0300

    added unique email flag

--- lib/resources/parent/parentModel.ts
+++ lib/resources/parent/parentModel.ts
@@ -3,7 +3,7 @@ import {createSchema, Type, typedModel} from "ts-mongoose";
 const parentSchema = createSchema
 ({
     name: Type.string( {required: true} ),
-    email: Type.string( {required: true}),
+    email: Type.string( {required: true, unique: true }),
     password: Type.string( {required: true}),
 });
 
